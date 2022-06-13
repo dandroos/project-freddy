20988d534b2c7c6c679c7b418794855dda3ed5ca
Dave Androos <daveandrooses@gmail.com>
2022-06-13 16:24:26 +0100

    reverted to prev colours

--- style.js
+++ style.js
@@ -1,9 +1,9 @@
 module.exports = {
   palette: {
-    main: "#83329A",
+    main: "#30287a",
     // main: "#a28945",
     // main: "#443087",
-    secondary: "#D86B18",
+    secondary: "#ac3413",
     // secondary: "#816C37",
     // secondary: "#ba402b",
     light: "#EBE3E0",
